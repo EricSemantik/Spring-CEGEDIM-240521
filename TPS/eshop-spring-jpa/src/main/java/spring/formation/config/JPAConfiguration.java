f6ad880d5b70cddb9b36ae8cc753c4f3bdfe4e62
AzureAD\EricSULTAN <eric@fifox.fr>
2024-05-22 17:02:50 +0200

    SPRING JPA TP avec Properties Correction

--- TPS/eshop-spring-jpa/src/main/java/spring/formation/config/JPAConfiguration.java
+++ TPS/eshop-spring-jpa/src/main/java/spring/formation/config/JPAConfiguration.java
@@ -5,9 +5,13 @@ import java.util.Properties;
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
@@ -20,16 +24,21 @@ import jakarta.persistence.EntityManagerFactory;
 @Configuration
 @ComponentScan("spring.formation.repository.jpa")
 @EnableTransactionManagement
+@PropertySource("classpath:datasource.properties")
 public class JPAConfiguration {
+	@Autowired
+	private Environment env;
+	
 	@Bean
 	public DataSource dataSource() {
 		BasicDataSource dataSource = new BasicDataSource();
-		
-		dataSource.setDriverClassName("org.postgresql.Driver");
-		dataSource.setUrl("jdbc:postgresql://localhost:5432/eshop");
-		dataSource.setUsername("postgres");
-		dataSource.setPassword("admin");
-		dataSource.setMaxTotal(10);
+
+		dataSource.setDriverClassName(env.getProperty("db.driver"));
+		dataSource.setUrl(env.getProperty("db.url"));
+		dataSource.setUsername(env.getProperty("db.username"));
+		dataSource.setPassword(env.getProperty("db.password"));
+		dataSource.setMaxTotal(
+				!env.getProperty("db.maxTotal").isEmpty() ? Integer.valueOf(env.getProperty("db.maxTotal")) : 10);
 
 		return dataSource;
 	}
